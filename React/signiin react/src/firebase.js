1b5882bea90008cb1814c526a183b359f824fa69
Beratcan Acil <beratcan_acil@hotmail.se>
2018-09-15 22:18:47 +0200

    Update firebase.js

--- React/signiin react/src/firebase.js
+++ React/signiin react/src/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 // Initialize Firebase
 const config = {
-    apiKey: "AIzaSyBz5omD-G4sSZzVDoYeWXJRKd0XGuznBVY",
-    authDomain: "recept-1a5da.firebaseapp.com",
-    databaseURL: "https://recept-1a5da.firebaseio.com",
-    projectId: "recept-1a5da",
-    storageBucket: "recept-1a5da.appspot.com",
-    messagingSenderId: "829027743533"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
 };
 firebase.initializeApp(config);
 
